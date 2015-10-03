2e0b2b60d9da38de75dc055ac0a3f7702cc14033
JarrenBack <jarren.back@gmail.com>
2015-10-02 21:31:57 -0500

    Added user interface but not working yet

--- UserInterface.h
+++ UserInterface.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <iostream>
+#include "Polynomial.h"
 
+using namespace std;
+
+class UserInterface
+{
+public:
+	void displayMenu();
+	char getUserInput();
+	bool respondToUserInput(char input);
+	//Polynomial getPoly();
+private:
+
+};
